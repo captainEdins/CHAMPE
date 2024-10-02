7e67ce1feafe0af280ee55cca4f97f9ba3a0d35b
Edins <eddinsstivons@gmail.com>
2024-10-02 10:55:01 +0300

    data pushed

--- lib/resource/string.dart
+++ lib/resource/string.dart
@@ -1,17 +1,8 @@
 class Strings {
   static const String appName = "CHAMPE";
-  static const String selectBuy = "Please select account";
-  static const String company = "http://62.12.114.16/driver/";
-  static const String nearByPlace = "https://maps.googleapis.com/maps/api/place/nearbysearch/json";
-  static const String companyImages = "http://62.12.114.16/driver/uploads/images/";
-  static const String useDataExample = "Madison Blackstone is a director of brand marketing, with experience managing global teams and multimillion-dollar campaigns. Her background in brand strategy, visual design, and account management informs her mindful but competitive approach. Madison Blackstone is a director of brand marketing, with experience managing global teams and multimillion-dollar campaigns. Her background in brand strategy, visual design, and account management informs her mindful but competitive approach. Madison Blackstone is a director of brand marketing, with experience managing global teams and multimillion-dollar campaigns. Her background in brand strategy, visual design, and account management informs her mindful but competitive approach. Madison Blackstone is a director of brand marketing, with experience managing global teams and multimillion-dollar campaigns. Her background in brand strategy, visual design, and account management informs her mindful but competitive approach. Madison Blackstone is a director of brand marketing, with experience managing global teams and multimillion-dollar campaigns. Her background in brand strategy, visual design, and account management informs her mindful but competitive approach. ";
-  static const String useData = '';
-  static const String googleKey = 'AIzaSyAO6CcKrA0n1XTgIR6VHe-5G7P0p2KenGY';
+
   static const String version = '0.0.7';
-  static const String expiryNote = "Hello your $appName app have expired, please click the button below to download. Thank you.";
-  static const String collectData = "http://62.12.114.16/driver/uploads/images/";
-  static const String newApp = "http://62.12.114.16/App";
-  static const String discloseData = "http://62.12.114.16/driver/uploads/images/";
+
 
   static const String forgotPassword = "Forgot Password ?";
   static const String back = "Back";
@@ -22,7 +13,7 @@ class Strings {
   static const String registerNow = "Register now";
   static const String loginNow = "Sign in";
 
-  static const String key = "TURRDRRQ9UP5GZZ79YTSG8PMF";
+
   static const String userKemus = "Linda";
   static const String userGPT = "Linda";
   static const String BOT_NAME = "Linda";
