bdbf9c7ece073f33fe8172bf4bc3908759515cc0
Edins <eddinsstivons@gmail.com>
2024-10-02 10:59:08 +0300

    data pushed

--- lib/resource/string.dart
+++ lib/resource/string.dart
@@ -1,6 +1,6 @@
 class Strings {
   static const String appName = "CHAMPE";
-
+  static const String googleKey = 'AIzaSyAO6CcKrA0n1XTgIR6VHe-5G7P0p2KenGY';
   static const String version = '0.0.7';
 
 
@@ -13,7 +13,7 @@ class Strings {
   static const String registerNow = "Register now";
   static const String loginNow = "Sign in";
 
-
+  static const String key = "TURRDRRQ9UP5GZZ79YTSG8PMF";
   static const String userKemus = "Linda";
   static const String userGPT = "Linda";
   static const String BOT_NAME = "Linda";
